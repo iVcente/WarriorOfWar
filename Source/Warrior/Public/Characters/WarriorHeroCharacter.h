4e787e0ad539a3c3c34c7ccb22afbee48b5c4478
Vicente <ivcente@gmail.com>
2025-04-29 20:09:17 -0300

    Project setup

---
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -0,0 +1,37 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "WarriorHeroCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+	
+	public:
+		AWarriorHeroCharacter();
+	
+	protected:
+		virtual void BeginPlay() override;
+ 
+	private:
+ 
+		#pragma region Components
+ 		
+			UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+			USpringArmComponent* CameraBoom;
+		 
+			UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+			UCameraComponent* FollowCamera;
+ 
+		#pragma endregion
+};
--- Source/WarriorOfWar/WarriorOfWar.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WarriorOfWar.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarriorOfWar, "WarriorOfWar" );
